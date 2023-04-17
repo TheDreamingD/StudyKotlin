06c8dfd559afd30a958f7b84b05de179d799451d
DreamingDragon <thedreamingd@gmail.com>
2023-04-17 15:48:12 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 1: 시작하기 Task: Create Hello Kotlin - 프로젝트 생성

---
+++ Developer_Kotlin_IntelliJ/HelloKotlin/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+group = "kr.dragon.dreaming"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(14)
+}
+
+application {
+    mainClass.set("MainKt")
+}
