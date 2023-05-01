4278446153019ddbc307bb3218524caad35c869f
DreamingDragon <thedreamingd@gmail.com>
2023-05-01 23:06:46 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 4: 클래스 및 객체 Task: Create a class - 완료

---
+++ Developer_Kotlin_IntelliJ/OOP/src/main/kotlin/example/myapp/Aquarium.kt
@@ -0,0 +1,11 @@
+package example.myapp
+
+class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+    fun printSize() {
+        println("Witdh: $width cm Length: $length cm Height: $height cm")
+    }
+}
---
+++ Developer_Kotlin_IntelliJ/OOP/src/main/kotlin/example/myapp/Main1.kt
@@ -0,0 +1,12 @@
+package example.myapp
+
+fun main() {
+    buildAquarium()
+}
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+    myAquarium.height = 60
+    myAquarium.printSize()
+}
