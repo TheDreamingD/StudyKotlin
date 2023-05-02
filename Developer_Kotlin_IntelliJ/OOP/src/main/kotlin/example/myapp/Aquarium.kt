b9a580ccc2f9ed84015a1e9370ffa42e4743e578
DreamingDragon <thedreamingd@gmail.com>
2023-05-02 21:44:30 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 4: 클래스 및 객체 Task: Add class constructors - 완료

--- Developer_Kotlin_IntelliJ/OOP/src/main/kotlin/example/myapp/Aquarium.kt
+++ Developer_Kotlin_IntelliJ/OOP/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,11 +1,28 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    var volume: Int
+        get() = width * height * length / 1000 // 1000 cm^3 = 1 l
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+
+        // calculate the height needed
+        height = (tank / (length * width)).toInt()
+    }
+
+    init {
+        println("aquarium initializing")
+    }
 
     fun printSize() {
         println("Witdh: $width cm Length: $length cm Height: $height cm")
+
+        // 1 l = 1000 cm^3
+        println("Volume: $volume l")
     }
 }
--- Developer_Kotlin_IntelliJ/OOP/src/main/kotlin/example/myapp/Main1.kt
+++ Developer_Kotlin_IntelliJ/OOP/src/main/kotlin/example/myapp/Main1.kt
@@ -9,4 +9,18 @@ fun buildAquarium() {
     myAquarium.printSize()
     myAquarium.height = 60
     myAquarium.printSize()
+
+    val aquarium1 = Aquarium()
+    aquarium1.printSize()
+    val aquarium2 = Aquarium(width = 25)
+    aquarium2.printSize()
+    val aquarium3 = Aquarium(height = 35, length = 110)
+    aquarium3.printSize()
+    val aquarium4 = Aquarium(width = 25, height = 35, length = 110)
+    aquarium4.printSize()
+
+    val aquarium6 = Aquarium(numberOfFish = 29)
+    aquarium6.printSize()
+    aquarium6.volume = 70
+    aquarium6.printSize()
 }
