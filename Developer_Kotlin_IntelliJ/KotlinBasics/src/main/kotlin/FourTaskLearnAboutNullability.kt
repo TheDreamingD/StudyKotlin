b2d719bb344933b47e7ee1040c627705453ef67b
DreamingDragon <thedreamingd@gmail.com>
2023-06-14 17:03:49 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 2: Kotlin 기본사항 4. Task: Learn about nullability - 완료

---
+++ Developer_Kotlin_IntelliJ/KotlinBasics/src/main/kotlin/FourTaskLearnAboutNullability.kt
@@ -0,0 +1,18 @@
+fun main() {
+    // Step 1: Learn about nullability
+//    var rocks: Int = null
+    var marbles: Int? = null
+
+    // Step 2: Learn about the ? and ?: operators
+    var fishFoodTreats = 6
+    if (fishFoodTreats != null) {
+        fishFoodTreats = fishFoodTreats.dec()
+    }
+
+    fishFoodTreats = fishFoodTreats?.dec()
+
+    fishFoodTreats = fishFoodTreats?.dec() ?: 0
+
+    // A point about null pointers
+    val len = s!!.length
+}
