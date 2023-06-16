3d6d5fab82ad231c6e6e1189255be095f823bc9c
DreamingDragon <thedreamingd@gmail.com>
2023-06-16 16:30:11 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수 4. Task: Learn more about functions - 진행중

---
+++ Developer_Kotlin_IntelliJ/Functions/src/main/kotlin/FourTaskLearnMoreAboutFunctions.kt
@@ -0,0 +1,16 @@
+import java.util.*
+
+fun main() {
+    feedTheFish()
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and the fish eat $food")
+}
+
+fun randomDay(): String {
+    val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
