1cf26b1140010e3b6d2169704396da80d5d58e14
DreamingDragon <thedreamingd@gmail.com>
2023-06-16 16:38:31 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수 4. Task: Learn more about functions - 진행중

--- Developer_Kotlin_IntelliJ/Functions/src/main/kotlin/FourTaskLearnMoreAboutFunctions.kt
+++ Developer_Kotlin_IntelliJ/Functions/src/main/kotlin/FourTaskLearnMoreAboutFunctions.kt
@@ -1,16 +1,32 @@
 import java.util.*
 
 fun main() {
+    // Step 1: Create some functions
     feedTheFish()
 }
 
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println("Today is $day and the fish eat $food")
 }
 
 fun randomDay(): String {
     val week = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(week.size)]
+}
+
+// Step 2: Use a when expression
+fun fishFood(day: String): String {
+    var food = ""
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Tuesday" -> food = "pellets"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Saturday" -> food = "lettuce"
+        "Sunday" -> food = "plankton"
+    }
+    return food
 }
