ede9605bd7a055169befd4802f3ad4bcceea1335
DreamingDragon <thedreamingd@gmail.com>
2023-06-19 17:20:01 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수 7. Task: Get started with lambdas and higher-order functions - 완료

--- Developer_Kotlin_IntelliJ/Functions/src/main/kotlin/SevenTaskGetStartedWithLambdasAndHigherOrderFunctions.kt
+++ Developer_Kotlin_IntelliJ/Functions/src/main/kotlin/SevenTaskGetStartedWithLambdasAndHigherOrderFunctions.kt
@@ -1,6 +1,21 @@
 fun main() {
     // Step 1: Learn about lambdas
     var dirtyLevel = 20
-    val waterFilter = { dirty: Int -> dirty / 2 }
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
     println(waterFilter(dirtyLevel))
+
+    // Step 2: Create a higher-order function
+    println(updateDirty(30, waterFilter))
+
+    println(updateDirty(15, ::increaseDirty))
+
+    dirtyLevel = 19
+    dirtyLevel = updateDirty(dirtyLevel) { dirtyLevel -> dirtyLevel + 23 }
+    println(dirtyLevel)
 }
+
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
+
+fun increaseDirty(start: Int) = start + 1
