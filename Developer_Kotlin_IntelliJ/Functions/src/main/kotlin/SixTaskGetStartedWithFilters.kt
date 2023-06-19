ab59e2e7bce63cfd0a402d71935431b769c2ebe8
DreamingDragon <thedreamingd@gmail.com>
2023-06-19 16:29:17 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수 6. Task: Get started with filters - 완료

--- Developer_Kotlin_IntelliJ/Functions/src/main/kotlin/SixTaskGetStartedWithFilters.kt
+++ Developer_Kotlin_IntelliJ/Functions/src/main/kotlin/SixTaskGetStartedWithFilters.kt
@@ -1,6 +1,33 @@
-val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-
 fun main() {
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
     // Step 1: Create a filter
     println(decorations.filter { it[0] == 'p' })
+
+    // Step 2: Compare eager and lazy filters
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+    println("lazy: $lazyMap")
+    println("------")
+    println("first: ${ lazyMap.first() }")
+    println("------")
+    println("all: ${ lazyMap.toList() }")
+
+    val lazyMap2 = decorations.asSequence().filter { it[0] == 'p' }.map {
+        println("access: $it")
+        it
+    }
+    println("------")
+    println("filtered: ${ lazyMap2.toList() }")
 }
