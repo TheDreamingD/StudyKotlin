2c5986f4c5e6479366f1a58ecee291d410ebd5ac
DreamingDragon <thedreamingd@gmail.com>
2023-06-23 15:22:00 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 5.1: 확장 3. Task: Comprehend collections - 완료

---
+++ Developer_Kotlin_IntelliJ/Extensions/src/main/kotlin/ThreeTaskComprehendCollections.kt
@@ -0,0 +1,27 @@
+fun main() {
+    val list = listOf(1, 5, 3, 4)
+    println(list.sum())
+
+    val list2 = listOf("a", "bbb", "cc")
+//    println(list2.sum())
+
+    println(list2.sumBy { it.length })
+
+    for (s in list2.listIterator()) {
+        println("$s ")
+    }
+
+    val cures = hashMapOf("white spots" to "Ich", "red sores" to "hole disease")
+    println(cures.get("white spots"))
+    println(cures["red sores"])
+    println(cures["scale loss"])
+
+    println(cures.getOrDefault("bloating", "sorry, I don't know"))
+    println(cures.getOrElse("bloating") { "No cure for this" })
+
+    val inventory = mutableMapOf("fish net" to 1)
+    inventory.put("tank scrubber", 3)
+    println(inventory.toString())
+    inventory.remove("fish net")
+    println(inventory.toString())
+}
