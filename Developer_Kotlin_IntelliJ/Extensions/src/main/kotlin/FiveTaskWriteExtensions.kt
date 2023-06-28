8a275e7f4b32ef393e9989af297300d45c4092f3
DreamingDragon <thedreamingd@gmail.com>
2023-06-28 13:34:54 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 5.1: 확장 5. Task: Write extensions - 완료

--- Developer_Kotlin_IntelliJ/Extensions/src/main/kotlin/FiveTaskWriteExtensions.kt
+++ Developer_Kotlin_IntelliJ/Extensions/src/main/kotlin/FiveTaskWriteExtensions.kt
@@ -1,10 +1,35 @@
 fun main() {
     println("Does it have spaces?".hasSpaces())
+
+    val plant = FiveGreenLeafyPlant(size = 18)
+    plant.print()
+    println("\n")
+    val aquariumPlant: FiveAquariumPlant = plant
+    aquariumPlant.print()
+
+    println(aquariumPlant.isGreen)
+
+    val plant2: FiveAquariumPlant? = null
+    plant2.pull()
 }
 
 fun String.hasSpaces() = find { it == ' ' } != null
 
-class FiveAquariumPlant(val color: String, private val size: Int)
+open class FiveAquariumPlant(val color: String, private val size: Int)
+
+class FiveGreenLeafyPlant(size: Int) : FiveAquariumPlant("green", size)
+
+fun FiveAquariumPlant.print() = println("FiveAquariumPlant")
+fun FiveGreenLeafyPlant.print() = println("FiveGreenLeafyPlant")
 
 fun FiveAquariumPlant.isRed() = color == "red"
 //fun FiveAquariumPlant.isBig() = size > 50
+
+val FiveAquariumPlant.isGreen: Boolean
+    get() = color == "green"
+
+fun FiveAquariumPlant?.pull() {
+    this?.apply {
+        println("removing $this")
+    }
+}
