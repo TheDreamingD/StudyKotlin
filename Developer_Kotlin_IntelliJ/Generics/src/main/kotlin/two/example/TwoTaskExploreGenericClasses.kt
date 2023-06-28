96433b06d3d2508dfb64f997f6be1ba14331ab1a
DreamingDragon <thedreamingd@gmail.com>
2023-06-28 15:38:12 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 5.2: 일반 2. Task: Explore generic classes - 진행중

--- Developer_Kotlin_IntelliJ/Generics/src/main/kotlin/two/example/TwoTaskExploreGenericClasses.kt
+++ Developer_Kotlin_IntelliJ/Generics/src/main/kotlin/two/example/TwoTaskExploreGenericClasses.kt
@@ -1,9 +1,20 @@
 package two.example
 
 fun main(args: Array<String>) {
+    genericsExample()
 }
 
 fun genericsExample() {
-    val aquarium = TwoAquarium(TwoTapWater())
+    val aquarium = TwoAquarium<TwoTapWater>(TwoTapWater())
+    println("water needs processing: ${ aquarium.waterSupply.needsProcessing }")
     aquarium.waterSupply.addChemicalCleaners()
+    println("water needs processing: ${ aquarium.waterSupply.needsProcessing }")
+
+    val aquarium2 = TwoAquarium("string")
+    println(aquarium2.waterSupply)
+
+    val aquarium3 = TwoAquarium(null)
+    if (aquarium3.waterSupply == null) {
+        println("waterSupply is null")
+    }
 }
