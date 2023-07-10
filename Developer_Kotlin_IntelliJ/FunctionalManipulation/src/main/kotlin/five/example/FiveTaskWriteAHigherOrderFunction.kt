eca36fbe650987f200b2847225380d645a27bec3
DreamingDragon <thedreamingd@gmail.com>
2023-07-10 16:11:07 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 6: 함수 조작 5. Task: Write a higher-order function - 완료

--- Developer_Kotlin_IntelliJ/FunctionalManipulation/src/main/kotlin/five/example/FiveTaskWriteAHigherOrderFunction.kt
+++ Developer_Kotlin_IntelliJ/FunctionalManipulation/src/main/kotlin/five/example/FiveTaskWriteAHigherOrderFunction.kt
@@ -9,4 +9,14 @@ fun fishExamples() {
     myWith(fish.name) {
         println(capitalize())
     }
+
+    println(fish.run { name })
+
+    val fish2 = FiveFish(name = "splashy").apply {
+        name = "sharky"
+    }
+    println(fish2.name)
+
+    println(fish.let { it.name.capitalize() }.let { it + "fish" }.let { it.length }.let { it + 31 })
+    println(fish)
 }
