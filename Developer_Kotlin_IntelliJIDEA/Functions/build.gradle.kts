c2ae3751601b78b8b3f5079f452fddbd274583c2
DreamingDragon <thedreamingd@gmail.com>
2023-12-14 21:23:01 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수 2. Task: Explore the main() function - 프로젝트 생성

---
+++ Developer_Kotlin_IntelliJIDEA/Functions/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "kr.dragon.dreaming"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
