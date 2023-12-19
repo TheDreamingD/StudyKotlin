f794573bbc0ab78111d3cddc234301c99ae58f3d
DreamingDragon <thedreamingd@gmail.com>
2023-12-20 01:46:50 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수 3. Task: Learn why (almost) everything has a value - 완료

---
+++ Developer_Kotlin_IntelliJIDEA/Functions/src/main/kotlin/three/ThreeTaskLearnWhyAlmostEverythingHasAValue.kt
@@ -0,0 +1,13 @@
+package three
+
+fun main() {
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    val temperature = 10
+    val isHot = if (temperature > 50) true else false
+    println(isHot)
+
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
+}
