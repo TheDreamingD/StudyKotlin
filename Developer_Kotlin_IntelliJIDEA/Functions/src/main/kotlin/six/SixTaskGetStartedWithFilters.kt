5969d0fdc4bb4b6211fbd9a7be4b8c3f87a4053b
DreamingDragon <thedreamingd@gmail.com>
2024-01-05 00:23:32 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수 6. Task: Get started with filters - 완료

--- Developer_Kotlin_IntelliJIDEA/Functions/src/main/kotlin/six/SixTaskGetStartedWithFilters.kt
+++ Developer_Kotlin_IntelliJIDEA/Functions/src/main/kotlin/six/SixTaskGetStartedWithFilters.kt
@@ -1,7 +1,32 @@
 package six
 
-val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-
 fun main() {
-    println(decorations.filter { it[0] == 'p' })
+    val decorations = listOf("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
+
+    val eager = decorations.filter { it[0] == 'p' }
+    println("eager: $eager")
+
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
+
+    val newList = filtered.toList()
+    println("new list: $newList")
+
+    val lazyMap = decorations.asSequence().map {
+        println("access: $it")
+        it
+    }
+
+    println("lazy: $lazyMap")
+    println("-------")
+    println("first: ${ lazyMap.first() }")
+    println("-------")
+    println("all: ${ lazyMap.toList() }")
+
+    val lazyMap2 = decorations.asSequence().filter { it[0] == 'p' }.map {
+        println("access: $it")
+        it
+    }
+    println("-------")
+    println("filtered: ${ lazyMap2.toList() }")
 }
