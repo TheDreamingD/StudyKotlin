c72c2082fddfebb819c8b57ff70bb91ec4b04e4d
DreamingDragon <thedreamingd@gmail.com>
2024-02-16 15:23:19 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 5.1: 확장 2. Task: Learn about pairs and triples - 완료

---
+++ Developer_Kotlin_IntelliJIDEA/Extensions/src/main/kotlin/two/TwoTaskLearnAboutPairsAndTriples.kt
@@ -0,0 +1,20 @@
+package two
+
+fun main() {
+    val equipment = "fish net" to "catching fish"
+    println("${ equipment.first } used for ${ equipment.second }")
+
+    val numbers = Triple(6, 9, 42)
+    println(numbers.toString())
+    println(numbers.toList())
+
+    val equipment2 = ("fish net" to "catching fish") to "equipment"
+    println("${ equipment2.first } is ${ equipment2.second }\n")
+    println("${ equipment2.first.second }")
+
+    val (tool, use) = equipment
+    println("$tool is used for $use")
+
+    val (n1, n2, n3) = numbers
+    println("$n1 $n2, $n3")
+}
