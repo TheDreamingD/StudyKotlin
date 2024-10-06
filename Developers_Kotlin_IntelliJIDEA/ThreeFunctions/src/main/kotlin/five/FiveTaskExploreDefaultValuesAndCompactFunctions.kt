7d9ff7e83d00ad4b2584d54ab5ccb8b75d1c8c7f
DreamingDragon <thedreamingd@gmail.com>
2024-10-06 16:31:02 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 3: 함수(Functions) 5. Task: Explore default values and compact functions - 완료

--- Developers_Kotlin_IntelliJIDEA/ThreeFunctions/src/main/kotlin/five/FiveTaskExploreDefaultValuesAndCompactFunctions.kt
+++ Developers_Kotlin_IntelliJIDEA/ThreeFunctions/src/main/kotlin/five/FiveTaskExploreDefaultValuesAndCompactFunctions.kt
@@ -16,12 +16,18 @@ fun swim(speed: String = "fast") {
 
 fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
     return when {
-        temperature > 30 -> true
-        dirty > 30 -> true
-        day == "Sunday" -> true
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
         else -> false
     }
 }
+
+fun isTooHot(temperature: Int) = temperature > 30
+
+fun isDirty(dirty: Int) = dirty > 30
+
+fun isSunday(day: String) = day == "Sunday"
 
 fun feedTheFish() {
     val day = randomDay()
