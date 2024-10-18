17e3d59727ea883dbfd1bc1161ef8eace32df1f3
DreamingDragon <thedreamingd@gmail.com>
2024-10-18 17:25:12 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 4: 클래스 및 객체(Object-oriented programming) 4. Task: Add class constructors - 진행중

--- Developers_Kotlin_IntelliJIDEA/FourObjectOrientedProgramming/src/main/kotlin/four/example/myapp/FourAquarium.kt
+++ Developers_Kotlin_IntelliJIDEA/FourObjectOrientedProgramming/src/main/kotlin/four/example/myapp/FourAquarium.kt
@@ -1,9 +1,18 @@
 package four.example.myapp
 
-class FourAquarium(length: Int = 100, width: Int = 20, height: Int = 40) {
-    var length: Int = length
-    var width: Int = width
-    var height: Int = height
+class FourAquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        println("Volume: ${ width * length * height / 1000 } l")
+    }
+
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
 
     fun printSize() {
         println("Width: $width cm, Length: $length cm, Height: $height cm")
