4c3a64d13cb52cfc26690d443f672d82ca12b0fe
DreamingDragon <thedreamingd@gmail.com>
2024-10-22 17:47:10 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 4: 클래스 및 객체(Object-oriented programming) 6. Task: Learn about subclasses and inheritance - 준비

---
+++ Developers_Kotlin_IntelliJIDEA/FourObjectOrientedProgramming/src/main/kotlin/six/example/myapp/SixAquarium.kt
@@ -0,0 +1,24 @@
+package six.example.myapp
+
+class SixAquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+    var volume: Int
+        get() = width * height * length / 1000
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
+
+    init {
+        println("aquarium initializing")
+    }
+
+    constructor(numberOfFish: Int) : this() {
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
+
+    fun printSize() {
+        println("Width: $width cm, Length: $length cm, Height: $height cm")
+
+        println("Volume: $volume l")
+    }
+}
