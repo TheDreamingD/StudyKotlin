e44d3904c58ad1d2068a7e2728e4a8cfa8db9333
DreamingDragon <thedreamingd@gmail.com>
2024-10-23 13:45:32 +0900

    프로그래머를 위한 Kotlin 부트캠프 과정 4: 클래스 및 객체(Object-oriented programming) 6. Task: Learn about subclasses and inheritance - 완료

--- Developers_Kotlin_IntelliJIDEA/FourObjectOrientedProgramming/src/main/kotlin/six/example/myapp/SixTaskLearnAboutSubclassesAndInheritance.kt
+++ Developers_Kotlin_IntelliJIDEA/FourObjectOrientedProgramming/src/main/kotlin/six/example/myapp/SixTaskLearnAboutSubclassesAndInheritance.kt
@@ -5,6 +5,8 @@ fun main() {
 }
 
 fun buildAquarium() {
-    val aquarium6 = SixAquarium(length = 25, width = 25, height = 40)
-    aquarium6.printSize()
+    val myAquarium = SixAquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = SixTowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
