862f653735415c1e5246548b5d08dc3784221c8a
DreamingDragon <thedreamingd@gmail.com>
2025-07-26 15:49:01 +0900

    Android Basics with Compose Unit 1: Your first Android app PATHWAY1: Introduction to programming in Kotlin 4. Create and use variables in Kotlin 3. Define and use variables - 준비

---
+++ Developers_Kotlin_IntelliJIDEA/Android_Basics_with_Compose/Unit_1/PATHWAY1/FourCreateAndUseVariablesInKotlin/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+}
+
+group = "kr.dragon.dreaming"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
