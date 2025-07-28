f55c1f712e9916ac14ab5191b51500ed041d608e
DreamingDragon <thedreamingd@gmail.com>
2025-07-28 17:09:25 +0900

    Android Basics with Compose Unit 1: Your first Android app PATHWAY1: Introduction to programming in Kotlin 4. Create and use variables in Kotlin 3. Define and use variables - 완료

--- Developers_Kotlin_IntelliJIDEA/Android_Basics_with_Compose/Unit_1/PATHWAY1/FourCreateAndUseVariablesInKotlin/src/main/kotlin/Main.kt
+++ Developers_Kotlin_IntelliJIDEA/Android_Basics_with_Compose/Unit_1/PATHWAY1/FourCreateAndUseVariablesInKotlin/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 fun main() {
-    val unreadCount = 5
-    val readCount = 100
-    println("You have ${ unreadCount + readCount } total messages in your inbox.")
+    val numberOfPhotos = 100
+    val photosDeleted = 10
+    println("$numberOfPhotos photos")
+    println("$photosDeleted photos deleted")
+    println("${ numberOfPhotos - photosDeleted } photos left")
 }
