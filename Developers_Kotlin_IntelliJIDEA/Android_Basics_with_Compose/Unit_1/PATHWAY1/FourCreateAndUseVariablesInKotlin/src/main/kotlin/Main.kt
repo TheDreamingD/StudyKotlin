2fccd58c3f3dc624f739bcbd3614b1ecbcd09a61
DreamingDragon <thedreamingd@gmail.com>
2025-08-01 15:28:40 +0900

    Android Basics with Compose Unit 1: Your first Android app PATHWAY1: Introduction to programming in Kotlin 4. Create and use variables in Kotlin 5. Explore other data types - 진행중

--- Developers_Kotlin_IntelliJIDEA/Android_Basics_with_Compose/Unit_1/PATHWAY1/FourCreateAndUseVariablesInKotlin/src/main/kotlin/Main.kt
+++ Developers_Kotlin_IntelliJIDEA/Android_Basics_with_Compose/Unit_1/PATHWAY1/FourCreateAndUseVariablesInKotlin/src/main/kotlin/Main.kt
@@ -2,6 +2,6 @@ fun main() {
     val trip1: Double = 3.20
     val trip2: Double = 4.10
     val trip3: Double = 1.72
-    val totalTripLength: Double = 0.0
+    val totalTripLength: Double = trip1+ trip2 + trip3
     println("$totalTripLength miles left to destination")
 }
