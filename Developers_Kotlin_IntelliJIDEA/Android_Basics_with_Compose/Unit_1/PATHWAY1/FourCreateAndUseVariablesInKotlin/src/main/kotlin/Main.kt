686e991da92e4e41104da7e18a8a95f428e9e957
DreamingDragon <thedreamingd@gmail.com>
2025-08-01 15:24:17 +0900

    Android Basics with Compose Unit 1: Your first Android app PATHWAY1: Introduction to programming in Kotlin 4. Create and use variables in Kotlin 5. Explore other data types - 진행중

--- Developers_Kotlin_IntelliJIDEA/Android_Basics_with_Compose/Unit_1/PATHWAY1/FourCreateAndUseVariablesInKotlin/src/main/kotlin/Main.kt
+++ Developers_Kotlin_IntelliJIDEA/Android_Basics_with_Compose/Unit_1/PATHWAY1/FourCreateAndUseVariablesInKotlin/src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 fun main() {
-    var count: Int = 10
-    println("You have $count unread messages.")
-    count--
-    println("You have $count unread messages.")
+    val trip1: Double = 3.20
+    val trip2: Double = 4.10
+    val trip3: Double = 1.72
+    val totalTripLength: Double = 0.0
+    println("$totalTripLength miles left to destination")
 }
