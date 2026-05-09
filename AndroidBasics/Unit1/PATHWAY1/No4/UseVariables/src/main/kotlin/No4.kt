1d899b0ab8cf43228e81c59e1a1246231ae78061
DreamingDragon <thedreamingd@gmail.com>
2026-05-09 23:30:02 +0900

    Compose 사용 시 알아야 하는 Android 기본사항 단원 1: 첫 번째 Android 앱 과정 1: Kotlin 소개 4. Kotlin에서 변수 만들기 및 사용하기 4. 변수 업데이트 - 진행중

--- AndroidBasics/Unit1/PATHWAY1/No4/UseVariables/src/main/kotlin/No4.kt
+++ AndroidBasics/Unit1/PATHWAY1/No4/UseVariables/src/main/kotlin/No4.kt
@@ -1,7 +1,4 @@
 fun main() {
-    var cartTotal = 0
-    println("Total: $cartTotal")
-
-    cartTotal = 20
-    println("Total: $cartTotal")
+    val count: Int = 10
+    println("You have $count unread messages.")
 }
