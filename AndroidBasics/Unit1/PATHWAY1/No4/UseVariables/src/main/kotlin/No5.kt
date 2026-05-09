d487b40d481e7a6c103081eb3897a704a2076979
DreamingDragon <thedreamingd@gmail.com>
2026-05-10 00:08:51 +0900

    Compose 사용 시 알아야 하는 Android 기본사항 단원 1: 첫 번째 Android 앱 과정 1: Kotlin 소개 4. Kotlin에서 변수 만들기 및 사용하기 5. 다른 데이터 유형 살펴보기 - 진행중

--- AndroidBasics/Unit1/PATHWAY1/No4/UseVariables/src/main/kotlin/No5.kt
+++ AndroidBasics/Unit1/PATHWAY1/No4/UseVariables/src/main/kotlin/No5.kt
@@ -1,7 +1,6 @@
 fun main() {
-    val trip1 = 3.20
-    val trip2 = 4.10
-    val trip3 = 1.72
-    val totalTripLength = trip1 + trip2 + trip3
-    println("$totalTripLength miles left to destination")
+    val nextMeeting = "Next meeting:"
+    val date = "January 1"
+    val reminder = nextMeeting + date
+    println(reminder)
 }
