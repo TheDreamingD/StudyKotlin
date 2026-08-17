717e4728f3c2affd79aabb1b865d7ebd0fe22f1c
DreamingDragon <thedreamingd@gmail.com>
2026-08-17 15:40:26 +0900

    Android Basics with Compose Unit 1: Your first Android app Introduction to programming in Kotlin 진행중

--- AndroidBasics/Unit1/PATHWAY1/Functions/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit1/PATHWAY1/Functions/src/main/kotlin/Main.kt
@@ -1,8 +1,11 @@
 fun main() {
-    birthdayGreeting()
+    val greeting = birthdayGreeting()
+    println(greeting)
 }
 
-fun birthdayGreeting(): Unit { // 아무것도 반환하지 않도록 하면 Unit을 반환한다.
-    println("Happy Birthday, Rover!")
-    println("You are now 5 years old!")
+fun birthdayGreeting(): String {
+    val nameGreeting = "Happy Birthday, Rover!"
+    val ageGreeting = "You are now 5 years old!"
+
+    return "$nameGreeting\n$ageGreeting"
 }
