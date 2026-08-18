3c82efed3e75198a9e751fed31572d21f40867b4
DreamingDragon <thedreamingd@gmail.com>
2026-08-19 01:02:44 +0900

    Android Basics with Compose Unit 2: Building app UI Kotlin fundamentals

--- AndroidBasics/Unit2/PATHWAY1/Classes/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit2/PATHWAY1/Classes/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
-class SmartDevice(val name: String, val category: String) { // 기본생성자는 헤더에 위치하고 본문을 가질 수 없음
+open/*open 키워드를 사용해야 이 클래스를 상속받는 하위클래스를 만들 수 있음*/ class SmartDevice(val name: String, val category: String) { // 기본생성자는 헤더에 위치하고 본문을 가질 수 없음
     var deviceStatus = "online"
+
+    open val deviceType = "unknown"
 
     /*
      * 보조생성자는 클래스 내에 위치하고 constructor로 시작하고, 기본생성자를 호출해주어야 한다.
@@ -12,12 +14,128 @@ class SmartDevice(val name: String, val category: String) { // 기본생성자�
         }
     }
 
-    fun turnOn() {
+    open fun turnOn() {
+        deviceStatus = "on"
         println("Smart device is turned on.")
     }
 
-    fun turnOff() {
+    open fun turnOff() {
+        deviceStatus = "off"
         println("Smart device is turned off.")
+    }
+}
+
+// smart tv IS-A smart device
+// 스마트디바이스가 할수있는 모든 일을 스마트티비가 할수 있다
+class SmartTvDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) { // 매개변수에 var, val을 지정하지 않았다는것은 부모에게 전달하는 역할만 한다는 것
+    override val deviceType = "Smart TV"
+
+    var speakerVolume = 2
+        set(value) {
+            if (value in 1..100) {
+                field = value // value는 할당하려는 값이고 이 값을 변수 이름에 할당하지 않고 field에 할당해야 한다.
+            }
+        }
+
+    var channelNumber = 1
+        set(value) {
+            if (value in 0..200) {
+                field = value
+            }
+        }
+
+    fun increaseSpeakerVolume() {
+        speakerVolume++
+        println("Speaker volume increased to $speakerVolume.")
+    }
+
+    fun nextChannel() {
+        channelNumber++
+        println("Channel number increased to $channelNumber.")
+    }
+
+    override fun turnOn() {
+        super.turnOn()
+        println(
+            "$name is turned on. Speaker volume is set to $speakerVolume and channel number is " +
+                    "set to $channelNumber."
+        )
+    }
+
+    override fun turnOff() {
+        super.turnOff()
+        println("$name turned off")
+    }
+}
+
+class SmartLightDevice(deviceName: String, deviceCategory: String) :
+    SmartDevice(name = deviceName, category = deviceCategory) {
+
+    override val deviceType = "Smart Light"
+
+    var brightnessLevel = 0
+        set(value) {
+            if (value in 0..100) {
+                field = value
+            }
+        }
+
+    fun increaseBrightness() {
+        brightnessLevel++
+        println("Brightness increased to $brightnessLevel.")
+    }
+
+    override fun turnOn() {
+        super.turnOn()
+        brightnessLevel = 2
+        println("$name turned on. The brightness level is $brightnessLevel.")
+    }
+
+    override fun turnOff() {
+        super.turnOff()
+        brightnessLevel = 0
+        println("Smart Light turned off")
+    }
+}
+
+// smart home HAS-A smart tv and smart light
+class SmartHome(
+    val smartTvDevice: SmartTvDevice,
+    val smartLightDevice: SmartLightDevice
+) {
+
+    fun turnOnTv() {
+        smartTvDevice.turnOn()
+    }
+
+    fun turnOffTv() {
+        smartTvDevice.turnOff()
+    }
+
+    fun increaseTvVolume() {
+        smartTvDevice.increaseSpeakerVolume()
+    }
+
+    fun changeTvChannelToNext() {
+        smartTvDevice.nextChannel()
+    }
+
+    fun turnOnLight() {
+        smartLightDevice.turnOn()
+    }
+
+    fun turnOffLight() {
+        smartLightDevice.turnOff()
+    }
+
+    fun increaseLightBrightness() {
+        smartLightDevice.increaseBrightness()
+    }
+
+    fun turnOffAllDevices() {
+        turnOffTv()
+        turnOffLight()
     }
 }
 
@@ -26,4 +144,10 @@ fun main() {
     println("Device name is: ${smartTvDevice.name}")
     smartTvDevice.turnOn()
     smartTvDevice.turnOff()
+
+    var smartDevice: SmartDevice = SmartTvDevice("Android TV", "Entertainment")
+    smartDevice.turnOn()
+
+    smartDevice = SmartLightDevice("Google Light", "Utility")
+    smartDevice.turnOn()
 }
