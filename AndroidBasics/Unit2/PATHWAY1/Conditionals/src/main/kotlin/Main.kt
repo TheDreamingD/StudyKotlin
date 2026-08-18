3b03458ec907ffbda65db231243211039b02f093
DreamingDragon <thedreamingd@gmail.com>
2026-08-18 13:39:13 +0900

    Android Basics with Compose Unit 2: Building app UI Kotlin fundamentals

---
+++ AndroidBasics/Unit2/PATHWAY1/Conditionals/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val trafficLightColor = "Yellow"
+
+    when (trafficLightColor) {
+        "Red" -> println("Stop")
+        "Yellow" -> println("Slow")
+        "Green" -> println("Go")
+        else -> println("Invalid traffic-light color")
+    }
+}
