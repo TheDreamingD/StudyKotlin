1adc8bd7d4c68653d076f5b50f74cce98a12daac
DreamingDragon <thedreamingd@gmail.com>
2026-08-18 13:47:47 +0900

    Android Basics with Compose Unit 2: Building app UI Kotlin fundamentals

--- AndroidBasics/Unit2/PATHWAY1/Conditionals/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit2/PATHWAY1/Conditionals/src/main/kotlin/Main.kt
@@ -7,4 +7,21 @@ fun main() {
         "Green" -> println("Go")
         else -> println("Invalid traffic-light color")
     }
+
+    val x = 4
+
+    when (x) {
+        2, 3, 5, 7 -> println("x is a prime number between 1 and 10.")
+        in 1..10 -> println("x is a number between 1 and 10, but not a prime number.")
+        else -> println("x isn't a prime number between 1 and 10.")
+    }
+
+    val xx: Any = 20
+
+    when (xx) {
+        2, 3, 5, 7 -> println("xx is a prime number between 1 and 10.")
+        in 1..10 -> println("xx is a number between 1 and 10, but not a prime number.")
+        is Int -> println("xx is an integer number, but not between 1 and 10.")
+        else -> println("x isn't an integer number.")
+    }
 }
