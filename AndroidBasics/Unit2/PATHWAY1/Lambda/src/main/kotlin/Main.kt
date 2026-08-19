2e1431a3d119c9dc3db973bab079ca54fb9e99a6
DreamingDragon <thedreamingd@gmail.com>
2026-08-19 15:03:28 +0900

    Android Basics with Compose Unit 2: Building app UI Kotlin fundamentals

--- AndroidBasics/Unit2/PATHWAY1/Lambda/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit2/PATHWAY1/Lambda/src/main/kotlin/Main.kt
@@ -6,16 +6,33 @@ fun main() {
 //    trickFunction()
 //    treat()
 
-    val treatFunction = trickOrTreat(false)
-    val trickFunction = trickOrTreat(true)
+    val coins: (Int) -> String = { quantity ->
+        "$quantity quarters" // return 키워드가 없고 함수의 마지막 표현식을 반환함
+    }
+    val cupcake: (Int) -> String = { // 함수의 입력이 Int가 있어도 매개변수를 따로 줄 필요가 없으면 매개변수는 생략해도 된다.
+        "Have a cupcake!"
+    }
+    val treatFunction = trickOrTreat(false, coins)
+    val trickFunction = trickOrTreat(true, null)
     treatFunction()
     trickFunction()
 }
 
-fun trickOrTreat(isTrick: Boolean): () -> Unit { // 함수의 리턴타입이 함수임
+//fun trickOrTreat(isTrick: Boolean): () -> Unit { // 함수의 리턴타입이 함수임
+//    if (isTrick) {
+//        return trick
+//    } else {
+//        return treat
+//    }
+//}
+
+fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit { // 함수 매개변수는 null을 허용하는 함수 타입이다.
     if (isTrick) {
         return trick
     } else {
+        if (extraTreat != null) {
+            println(extraTreat(5))
+        }
         return treat
     }
 }
