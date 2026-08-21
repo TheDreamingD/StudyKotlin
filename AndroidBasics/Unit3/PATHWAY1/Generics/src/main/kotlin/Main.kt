56fa408e5a7f8a6c6f877a4ab70a28b14861ec28
DreamingDragon <thedreamingd@gmail.com>
2026-08-21 23:53:21 +0900

    Android Basics with Compose Unit 3: Display lists and use Material Design More Kotlin fundamentals

--- AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
@@ -31,11 +31,27 @@ enum class Difficulty { // 값 집합을 제한할 때 사용. 값을 대분자�
     EASY, MEDIUM, HARD
 }
 
-class Quiz {
+interface ProgressPrintable {
+    val progressText: String
+
+    fun printProgressBar()
+}
+
+class Quiz : ProgressPrintable {
+
+    override val progressText: String
+        get() = "$answered of $total answered"
 
     val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
     val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
     val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
+
+    override fun printProgressBar() {
+        repeat(Quiz.answered) { print("*") }
+        repeat(Quiz.total - Quiz.answered) { print("#") }
+        println()
+        println(progressText)
+    }
 
     // 다른 클래스 내부에 싱글톤 객체를 만들때는 companion을 붙임
     // 아직은 어떤 장점이 있는지 모르겠음
@@ -45,27 +61,29 @@ class Quiz {
     }
 }
 
-val Quiz.StudentProgress.progressText: String // 클래스의 속성인 것처럼 클래스의 속성을 클래스 외부에서 확장해서 선언할 수 있다. 이것이 확장 속성이다.
-    get() ="$answered of $total answered"
+//val Quiz.StudentProgress.progressText: String // 클래스의 속성인 것처럼 클래스의 속성을 클래스 외부에서 확장해서 선언할 수 있다. 이것이 확장 속성이다.
+//    get() ="$answered of $total answered"
 
 //object StudentProgress { // 싱글톤 객체, class 대신 object 키워드 사용하면 됨
 //    var total: Int = 10
 //    var answered: Int = 3
 //}
 
-fun Quiz.StudentProgress.printProgressBar() { // 확장함수다. 마찬가지로 Quiz 클래스 밖에서 Quiz 클래스의 메서드인 것처럼 함수를 만들 수 있다.
-    repeat(Quiz.answered) { print("*") }
-    repeat(Quiz.total - Quiz.answered) { print("#") }
-    println()
-    println(Quiz.progressText)
-}
+//fun Quiz.StudentProgress.printProgressBar() { // 확장함수다. 마찬가지로 Quiz 클래스 밖에서 Quiz 클래스의 메서드인 것처럼 함수를 만들 수 있다.
+//    repeat(Quiz.answered) { print("*") }
+//    repeat(Quiz.total - Quiz.answered) { print("#") }
+//    println()
+//    println(Quiz.progressText)
+//}
 
 fun main() {
 //    println("${StudentProgress.answered} of ${StudentProgress.total} answered.") // 싱글톤클래스의 데이터에 접근하는 방법
     println("${Quiz.answered} of ${Quiz.total} answered.") // companion 객체의 데이터에 접근할때는 StudentProgress가 아닌 Quiz를 참조하여 접근함
 
 
-    println(Quiz.progressText) // progressText는 Quiz 클래스 외부에서 선언됐지만 Quiz 클래스 안의 속성인 것처럼 호출할 수 있다. 확장속성은 데이터를 저장할 수 없어서 get only다.
+//    println(Quiz.progressText) // progressText는 Quiz 클래스 외부에서 선언됐지만 Quiz 클래스 안의 속성인 것처럼 호출할 수 있다. 확장속성은 데이터를 저장할 수 없어서 get only다.
 
-    Quiz.printProgressBar()
+//    Quiz.printProgressBar()
+
+    Quiz().printProgressBar() // 이제 클래스 내의 메서드라서 Quiz를 생성하고 메서드를 호출해야 한다.
 }
