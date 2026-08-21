130135a6da95f72c5d14257dc2fea58df5602a0d
DreamingDragon <thedreamingd@gmail.com>
2026-08-21 14:39:04 +0900

    Android Basics with Compose Unit 3: Display lists and use Material Design More Kotlin fundamentals

--- AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
@@ -1,21 +1,30 @@
 // 각 클래스의 속성이 거의 같은데 클래스를 나누면 비효율적임
-class FillInTheBlankQuestion(
-    val questionText: String,
-    val answer: String,
-    val difficulty: String
-)
+//class FillInTheBlankQuestion(
+//    val questionText: String,
+//    val answer: String,
+//    val difficulty: String
+//)
+//
+//class TrueOrFalseQuestion(
+//    val questionText: String,
+//    val answer: Boolean,
+//    val difficulty: String
+//)
+//
+//class NumericQuestion(
+//    val questionText: String,
+//    val answer: Int,
+//    val difficulty: String
+//)
 
-class TrueOrFalseQuestion(
+class Question<T>( // 제네릭 타입 T를 사용해 위 3개의 클래스를 하나로 만듦
     val questionText: String,
-    val answer: Boolean,
-    val difficulty: String
-)
-
-class NumericQuestion(
-    val questionText: String,
-    val answer: Int,
+    val answer: T,
     val difficulty: String
 )
 
 fun main() {
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
 }
