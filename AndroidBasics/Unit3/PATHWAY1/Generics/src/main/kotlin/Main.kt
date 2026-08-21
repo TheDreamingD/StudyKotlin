3b8c6d8f9baa8013d414cc72d1fd19f2ea39e185
DreamingDragon <thedreamingd@gmail.com>
2026-08-21 14:32:24 +0900

    Android Basics with Compose Unit 3: Display lists and use Material Design More Kotlin fundamentals

--- AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
@@ -1,2 +1,21 @@
+// 각 클래스의 속성이 거의 같은데 클래스를 나누면 비효율적임
+class FillInTheBlankQuestion(
+    val questionText: String,
+    val answer: String,
+    val difficulty: String
+)
+
+class TrueOrFalseQuestion(
+    val questionText: String,
+    val answer: Boolean,
+    val difficulty: String
+)
+
+class NumericQuestion(
+    val questionText: String,
+    val answer: Int,
+    val difficulty: String
+)
+
 fun main() {
 }
