91efaf2903480ca83b8ec5c4f340b5ddba70966d
DreamingDragon <thedreamingd@gmail.com>
2026-08-21 14:50:47 +0900

    Android Basics with Compose Unit 3: Display lists and use Material Design More Kotlin fundamentals

--- AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit3/PATHWAY1/Generics/src/main/kotlin/Main.kt
@@ -20,11 +20,15 @@
 class Question<T>( // 제네릭 타입 T를 사용해 위 3개의 클래스를 하나로 만듦
     val questionText: String,
     val answer: T,
-    val difficulty: String
+    val difficulty: Difficulty
 )
 
+enum class Difficulty { // 값 집합을 제한할 때 사용. 값을 대분자로 정의
+    EASY, MEDIUM, HARD
+}
+
 fun main() {
-    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
-    val question2 = Question<Boolean>("The sky is green. True or false", false, "easy")
-    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", Difficulty.MEDIUM)
+    val question2 = Question<Boolean>("The sky is green. True or false", false, Difficulty.EASY)
+    val question3 = Question<Int>("How many days are there between full moons?", 28, Difficulty.HARD)
 }
