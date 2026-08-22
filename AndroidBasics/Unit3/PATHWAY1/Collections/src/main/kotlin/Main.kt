35401c10f135371a79293bfb9f718c0b67a8e76b
DreamingDragon <thedreamingd@gmail.com>
2026-08-23 00:16:50 +0900

    Android Basics with Compose Unit 3: Display lists and use Material Design More Kotlin fundamentals

--- AndroidBasics/Unit3/PATHWAY1/Collections/src/main/kotlin/Main.kt
+++ AndroidBasics/Unit3/PATHWAY1/Collections/src/main/kotlin/Main.kt
@@ -1,21 +1,45 @@
 fun main() {
     // 배열
-    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
-    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune") // 꺽쇠로 데이터타입을 지정하지 않아도 된다.
-    val solarSystem = rockPlanets + gasPlanets // 배열을 더하기로 합칠 수 있다.
+//    val rockPlanets = arrayOf<String>("Mercury", "Venus", "Earth", "Mars")
+//    val gasPlanets = arrayOf("Jupiter", "Saturn", "Uranus", "Neptune") // 꺽쇠로 데이터타입을 지정하지 않아도 된다.
+//    val solarSystem = rockPlanets + gasPlanets // 배열을 더하기로 합칠 수 있다.
+//
+//    println(solarSystem[0])
+//    println(solarSystem[1])
+//    println(solarSystem[2])
+//    println(solarSystem[3])
+//    println(solarSystem[4])
+//    println(solarSystem[5])
+//    println(solarSystem[6])
+//    println(solarSystem[7])
+//
+//    solarSystem[3] = "Little Earth"
+//    println(solarSystem[3])
+//
+//    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
+//    println(newSolarSystem[8])
 
-    println(solarSystem[0])
-    println(solarSystem[1])
+    // 리스트
+    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    println(solarSystem.size)
     println(solarSystem[2])
-    println(solarSystem[3])
-    println(solarSystem[4])
-    println(solarSystem[5])
-    println(solarSystem[6])
-    println(solarSystem[7])
+    println(solarSystem.get(3))
+    println(solarSystem.indexOf("Earth"))
+    println(solarSystem.indexOf("Pluto")) // 일치하는 항목이 없으면 -1을 반환
 
-    solarSystem[3] = "Little Earth"
-    println(solarSystem[3])
+    for (planet in solarSystem) {
+        println(planet)
+    }
 
-    val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
-    println(newSolarSystem[8])
+    // 요소를 추가하고 삭제하려면 mutable list로 만들어야 한다.
+    val solarSystem2 = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    solarSystem2.add("Pluto")
+    solarSystem2.add(3, "Theia")
+    solarSystem2[3] = "Future moon" // 요소의 값을 수정
+    println(solarSystem2[3])
+    println(solarSystem2[9])
+    solarSystem2.removeAt(9)
+    solarSystem2.remove("Future moon")
+    println(solarSystem2.contains("Pluto"))
+    println("Future moon" in solarSystem2)
 }
